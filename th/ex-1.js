32ad0f2c879ac50fef10962929e21ac61769263e
Suriya <true.many.032@gmail.com>
2025-04-04 11:41:40 +0700

    inclass-ex1,2

--- th/ex-1.js
+++ th/ex-1.js
@@ -1,6 +1,16 @@
 const moveZeroes = function (nums) {
-  //Start coding here
-};
+  let left = 0;
+  for (let right = 0; right < nums.length; right++) 
+  {
+    if (nums[right] !== 0) {
+      let temp = nums[left];
+      nums[left] = nums[right];
+      nums[right] = temp;
+      left++;
+    }
+  };
+  return nums;
+}
 
 const result1 = moveZeroes([0, 1, 0, 3, 12]);
 console.log(result1); // [1,3,12,0,0]
--- th/ex-2.js
+++ th/ex-2.js
@@ -1,5 +1,35 @@
 const romanToInt = function (s) {
-  //Start coding here
+  let result = 0;
+  for (let i = 0; s[i]; i++)
+  {
+    if (s[i] === 'I' && s[i + 1] === 'V')
+      result += 4, i++;
+    else if (s[i] === 'I' && s[i + 1] === 'X')
+      result += 9, i++;
+    else if (s[i] === 'X' && s[i + 1] === 'L')
+      result += 40, i++;
+    else if (s[i] === 'X' && s[i + 1] === 'C')
+      result += 90, i++;
+    else if (s[i] === 'C' && s[i + 1] === 'D')
+      result += 400, i++;
+    else if (s[i] === 'C' && s[i + 1] === 'M')
+      result += 900, i++;
+    else if (s[i] === 'I')
+      result += 1;
+    else if (s[i] === 'V')
+      result += 5;
+    else if (s[i] === 'X')
+      result += 10;
+    else if (s[i] === 'L')
+      result += 50;
+    else if (s[i] === 'C')
+      result += 100;
+    else if (s[i] === 'D')
+      result += 500;
+    else if (s[i] === 'M')
+      result += 1000;
+  }
+  return result;
 };
 
 const result1 = romanToInt("III"); // 3
@@ -10,3 +40,6 @@ console.log(result2);
 
 const result3 = romanToInt("MCMXCIV"); //1994
 console.log(result3);
+
+const result4 = romanToInt("IX");
+console.log(result4); // 9
